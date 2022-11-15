3d6f769e4b201e26f18498b97aef0e57f9b49344
Lawrence Mensah Ofoli <lawrenceofoli@gmail.com>
2022-11-15 15:19:50 +0000

    restructured app and removed unneccesary files and deps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Contacts from './components/contacts/Contacts';
-import AddContact from './components/contacts/AddContact';
-import EditContact from './components/contacts/EditContact';
-import Header from './components/layout/Header';
-import About from './components/pages/About';
-import NotFound from './components/pages/NotFound';
-import Test from './components/test/Test';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contacts from "./components/Contacts";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
+import Header from "./layout/Header";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
-import { Provider } from './context';
+import { Provider } from "./context/context";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -21,14 +20,13 @@ class App extends Component {
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">
-              <Switch>
-                <Route exact path="/" component={Contacts} />
-                <Route exact path="/contact/add" component={AddContact} />
-                <Route exact path="/contact/edit/:id" component={EditContact} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/test" component={Test} />
-                <Route component={NotFound} />
-              </Switch>
+              <Routes>
+                <Route exact path="/" element={<Contacts />} />
+                <Route path="/contact/add" element={<AddContact />} />
+                <Route path="/contact/edit/:id" element={<EditContact />} />
+                <Route path="/about" element={<About />} />
+                <Route element={<NotFound />} />
+              </Routes>
             </div>
           </div>
         </Router>
